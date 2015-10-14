de5751744f217ac79c7ac0135e446cb17e9a038d
Dan Vallejo <Dan.Vallejo@exchange.nordstrom.com>
2015-10-14 16:09:38 -0700

    add performRequest

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var querystring = require('querystring');
+
+var https = require('https');
+
+var host = 'localhost';
+
+function performRequest(endpoint, method, data, success) {
+  var dataString = JSON.stringify(data);
+  var headers = {};
+  
+  if (method == 'GET') {
+    endpoint += '?' + querystring.stringify(data);
+  }
+  else {
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length
+    };
+  }
+  
+  var options = {
+    host: 'configuration-service-0.elasticbeanstalk.com',
+	//port: 80,
+    path: '/config',
+    method: 'GET',
+    headers: headers
+  };
+
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+      console.log(responseString);
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
+}
+
 router.get('/', function(req, res, next) {
+  performRequest('/config', 'GET', {data: 'data'}, function(data){
+	  console.log(data);
+  });
+	
   res.render('index', { title: 'Express' });
 });
 
